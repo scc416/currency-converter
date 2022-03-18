f3597af5f8744ae00648dccc3d303907c6da6b3d
Siu <siuc@protonmail.com>
2022-03-18 18:27:09 -0400

    display list of available currencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const App = () => {
-  return <h2>Currency Converter</h2> ;
+  const [currencies, setCurrencies] = useState([]);
+  useEffect(async () => {
+    try {
+      const { data } = await axios.get(
+        "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json"
+      );
+      const properties = Object.values(data);
+      setCurrencies(properties);
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+  const lst = currencies.map((currency) => <li key={currency}>{currency}</li>);
+  return (
+    <>
+      <h2>Currency Converter</h2>
+      <ul>{lst}</ul>
+    </>
+  );
 };
 
 export default App;
