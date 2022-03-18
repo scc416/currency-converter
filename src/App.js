6728478d707a757ba4007284c06aabd1274c2b58
Siu <siuc@protonmail.com>
2022-03-18 17:34:10 -0400

    remove div with className App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      Hello WORLD
-    </div>
-  );
+  return "Hello World";
 }
 
 export default App;
