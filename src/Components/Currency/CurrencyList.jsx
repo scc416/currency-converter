2da61e3175de0524b46a6d8b310f11847ffdf5d8
Siu <siuc@protonmail.com>
2022-03-20 15:09:15 -0400

    modifying style

--- src/Components/Currency/CurrencyList.jsx
+++ src/Components/Currency/CurrencyList.jsx
@@ -1,10 +1,10 @@
 import CurrencyListItem from "./CurrencyListItem";
-
+import "./Currency.css"
 const lst = [1, 2];
 
 const CurrencyList = () => {
   const elms = lst.map((elm) => <CurrencyListItem />);
-  return <div>{elms}</div>;
+  return <div className="CurrencyList">{elms}</div>;
 };
 
 export default CurrencyList;
--- src/Components/Currency/CurrencyListItem.jsx
+++ src/Components/Currency/CurrencyListItem.jsx
@@ -1,5 +1,5 @@
 const CurrencyListItem = () => {
-  return <div>hello</div>
-}
+  return <div className="CurrencyListItem">hello</div>;
+};
 
 export default CurrencyListItem;
