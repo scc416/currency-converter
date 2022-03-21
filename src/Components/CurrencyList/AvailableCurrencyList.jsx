6860469339d041ef3c67e72487bb2827af7f5c22
Siu <siuc@protonmail.com>
2022-03-21 16:52:24 -0400

    show only the first few characters

--- src/Components/CurrencyList/AvailableCurrencyList.jsx
+++ src/Components/CurrencyList/AvailableCurrencyList.jsx
@@ -1,13 +1,20 @@
 import AvailableCurrencyListItem from "./AvailableCurrencyListItem";
 import { makeDisplayCurrencies } from "../../helper";
 
-const AvailableCurrencyList = ({ availableCurrencies, code }) => {
+const AvailableCurrencyList = ({ availableCurrencies, code: selectedCode }) => {
   const displayedCurrencies = makeDisplayCurrencies(availableCurrencies);
   const elms = displayedCurrencies.map(({ code, displayedStr }) => (
-    <AvailableCurrencyListItem {...{ key: code, code, displayedStr }} />
+    <AvailableCurrencyListItem
+      {...{
+        key: code,
+        code,
+        displayedStr:
+          selectedCode === code ? displayedStr.slice(0, 8) : displayedStr,
+      }}
+    />
   ));
 
-  return <select value={code}>{elms}</select>;
+  return <select value={selectedCode}>{elms}</select>;
 };
 
 export default AvailableCurrencyList;
