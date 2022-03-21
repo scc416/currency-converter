9381411cfe9c735ced456a84a5a7b1d4eeda17fe
Siu <siuc@protonmail.com>
2022-03-20 20:00:40 -0400

    change constants to lower case

---
+++ src/helper.js
@@ -0,0 +1,13 @@
+import { currenciesEmoji } from "./constants";
+
+const findCurrency = (lst, name, code) => {
+  const keys = Object.keys(lst);
+  const values = Object.values(lst);
+};
+
+export const makeAvailableCurrencyLst = (lst) => {
+  const availableCurrencyLst = [];
+  for (const currency of currenciesEmoji) {
+  }
+  return availableCurrencyLst;
+};
