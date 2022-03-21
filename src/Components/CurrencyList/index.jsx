e4bfaf77a65f086ce6c33d6f32e4510e8a31b401
Siu <siuc@protonmail.com>
2022-03-20 20:48:54 -0400

    display default value in input

--- src/Components/CurrencyList/index.jsx
+++ src/Components/CurrencyList/index.jsx
@@ -6,8 +6,8 @@ import useData from "../../hooks/useData";
 const CurrencyList = () => {
   const { availableCurrencies, currencies } = useData();
 
-  const elms = currencies.map(({ code, val }, index) => (
-    <CurrencyListItem {...{ key: index, availableCurrencies, code, val }} />
+  const elms = currencies.map(({ code, value }, index) => (
+    <CurrencyListItem {...{ key: index, availableCurrencies, code, value }} />
   ));
 
   return (
