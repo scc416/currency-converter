75b380ab4faf8b170889b06f06b8f6518b706056
Siu <siuc@protonmail.com>
2022-03-21 19:30:00 -0400

    refactor

--- src/Components/CurrencyList/AvailableCurrencyList.jsx
+++ src/Components/CurrencyList/AvailableCurrencyList.jsx
@@ -1,21 +1,14 @@
 import Select from "react-select";
 import { makeDisplayCurrencies, findCurrencyObj } from "../../helper";
-import useOpen from "../../hooks/useOpen";
 
 const AvailableCurrencyList = ({
   availableCurrencies,
   code: selectedCode,
   updateCurrency,
 }) => {
-  const { opened, onMenuOpen, onMenuClose } = useOpen();
-
   const displayedCurrencies = makeDisplayCurrencies(availableCurrencies);
   const options = displayedCurrencies.map(({ code, displayedStr }) => {
-    const displayText =
-      selectedCode === code && !opened
-        ? displayedStr.slice(0, 8)
-        : displayedStr;
-    return { value: code, label: displayText };
+    return { value: code, label: displayedStr };
   });
 
   const value = findCurrencyObj(options, selectedCode);
@@ -25,8 +18,6 @@ const AvailableCurrencyList = ({
       {...{
         value,
         options,
-        onMenuOpen,
-        onMenuClose,
         isSearchable: true,
         onChange: (e) => updateCurrency(e.value),
       }}
